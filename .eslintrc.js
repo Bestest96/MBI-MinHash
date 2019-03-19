264382da3c4ac4a1b76b2688ddb6b654d2940567
mily20001 <mily20001@gmail.com>
2019-03-19 20:30:31 +0100

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "indent": ["error", 4],
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MBI-MinHash
 A bioinformatics project implementing the MinHash algorithm which calculates an estimated similarity of sequences.
+
+## Start dev-server
+
+`npm start`
